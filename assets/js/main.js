94499cc9be6d09d3ba343c45ad3f8de526da5c7e
Paulo Henrique <ph55@hotmail.com>
2023-02-09 18:55:35 -0300

    skills

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
+
+function backend(){
+    var img01 = document.querySelector('#box01');
+    var img02 = document.querySelector('#box02');
+
+    img01.innerHTML = '<img id="img01"  src="./assets/img/icon-javascript.png" alt="">'
+    img02.innerHTML = '<img id="img02"  src="./assets/img/icon-python.png" alt="">'
+}
+
+function frontend(){
+    var img01 = document.querySelector('#box01');
+    var img02 = document.querySelector('#box02');
+    
+    img01.innerHTML = '<img id="img03" src="./assets/img/icon-frontend.jpg" alt="">'
+    img02.innerHTML = '<img id="img04" src="./assets/img/icon-react.png" alt="">'
+}

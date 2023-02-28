1474c2ea6b715144bf9d19e2838918b94fe80f9f
Paulo Henrique <ph55@hotmail.com>
2023-02-27 22:31:41 -0300

    Update Javascript
    Reestruturação na section "habilidades"

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,33 +10,6 @@ function menuShow() {
     }
 }
 
-// Botão Back End
-function backend(){
-    var img01 = document.querySelector('#box01');
-    var img02 = document.querySelector('#box02');
-
-    img01.innerHTML = '<img id="img01"  src="./assets/img/icon-javascript.png" alt="">'
-    img02.innerHTML = '<img id="img02"  src="./assets/img/icon-python.png" alt="">'
-}
-
-// Botão Front End
-function frontend(){
-    var img01 = document.querySelector('#box01');
-    var img02 = document.querySelector('#box02');
-    
-    img01.innerHTML = '<img id="img03" src="./assets/img/icon-frontend.jpg" alt="">'
-    img02.innerHTML = '<img id="img04" src="./assets/img/icon-react.png" alt="">'
-}
-
-// Botão Data Science
-function datascience(){
-    var img01 = document.querySelector('#box01');
-    var img02 = document.querySelector('#box02');
-
-    img01.innerHTML = '<img id="img05" src="./assets/img/inco-mysql.png" alt="">'
-    img02.innerHTML = '<img id="img06" src="./assets/img/icon-powerbi.png" alt="">'
-}
-
 // Oculta Botão Scroll
 function ocultarBtnTop(){
     const btn = document.querySelector('.btn-top');
